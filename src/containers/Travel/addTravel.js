b2cab19463437bcb48f63f730729235ea3c56f3b
totiferreira <aristoteles.ferreira@outlook.com>
2018-10-15 23:51:24 -0300

    edit page add Travel

--- src/containers/Travel/addTravel.js
+++ src/containers/Travel/addTravel.js
@@ -4,42 +4,30 @@ import TableWrapper from "../Tables/antTables/antTable.style";
 import CardWrapper, { Box } from "./index.style";
 import IntlMessages from "../../components/utility/intlMessages";
 import PageHeader from "../../components/utility/pageHeader";
-import { AutoComplete, Button, Form, Row, Col, Input, Switch, Icon, Tooltip, Select } from 'antd'
+import { AutoComplete, Button, Form, Row, Col, Input, Switch, Icon, Tooltip, Select, notification } from 'antd'
 import axios from '../../helpers/axios'
 
 const Option = AutoComplete.Option;
 const FormItem = Form.Item
 
-function renderOption(item) {
-    return (
-      <Option key={item.category} text={item.category}>
-        {item.query} 在
-        <a
-          href={`https://s.taobao.com/search?q=${item.query}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {item.category}
-        </a>
-        区块中
-        <span className="global-search-item-count">约 {item.count} 个结果</span>
-      </Option>
-    );
-}
-
-function onSelect(value) {
-    console.log(value);
-  }
-
 class addTravel extends Component {
     state = {
         travelInfo: {
-            driver_name: ''
+            driver_id: '',
+            vehicle_id: '',
+            carts_id: [],
+            itinerary_id: '',
+            departureDate: '',
+            arrivalDate: '',
+            description: '',
+            status: 'canceled',
         },
         driversData: [],
         vehiclesData: [],
         cartsData: [],
-        itinerariesData: []
+        itinerariesData: [],
+        name: '',
+        confirmLoading: false
     }
 
     componentWillMount = () => {
@@ -50,11 +38,42 @@ class addTravel extends Component {
     }
     onChangeAddTravelInfo(key, value) {
         this.setState({
-          driversInfo: {
+          travelInfo: {
             ...this.state.travelInfo,
             [key]: value
           }
+          
         });
+
+    }
+    addTravel = () => {
+        const { driver_id } = this.state.travelInfo;
+        if (driver_id !== '' /*&& cpf_number !== '' && drivers_license !== '' && driversLicense_validate !== ''*/) {
+          let newTravelInfo = {
+            ...this.state.travelInfo
+          };
+          axios.post("travels", newTravelInfo)
+            .then(response => {
+              this.setState({
+                confirmLoading: true
+              });
+              setTimeout(() => {
+                this.setState({
+                  confirmLoading: false
+                });
+                //this.handleAddClose();
+                //this.componentWillMount()
+              }, 2000);
+              notification.success({message: 'Cadastrado com sucesso !'})
+              
+            })
+            .catch(error => {
+              notification.error({message: 'Não foi possivel cadastrar !'})
+              console.log(error);
+            });
+        } else {
+          notification.warning({message: 'Campos inválidos', description: 'Preencha todos os campos obrigatórios (*)'})
+        }
     }
     getAllDrivers() {
         axios.get('drivers')
@@ -100,13 +119,14 @@ class addTravel extends Component {
             console.log(error)
         })
     }
+
+    handleSearch = (value) => {
+        fetch(value, data => this.setState({ data }));
+    }
     
+
     render() {
         const { getFieldDecorator } = this.props.form;
-        const drivers = this.state.driversData.map(driver => <Option key={driver.id}>{driver.name} | CPF: {driver.cpf_number}</Option>);
-        const vehicles = this.state.vehiclesData.map(vehicle => <Option key={vehicle.id}>{vehicle.brand} | Modelo: {vehicle.model} | Placa: {vehicle.plate}</Option>);
-        const carts = this.state.cartsData.map(cart => <Option key={cart.id}>{cart.brand} | Modelo: {cart.model} | Placa: {cart.plate}</Option>);
-        const itineraries = this.state.itinerariesData.map(itinerary => <Option key={itinerary.id}>{itinerary.route_name} | {itinerary.initial_point} até {itinerary.end_point}</Option>);
         return(
             <LayoutWrapper>
             <PageHeader>
@@ -129,9 +149,22 @@ class addTravel extends Component {
                                 })(
  
                                 <Select
+                                showSearch
+                                optionFilterProp="children"
                                 placeholder={'Escolha o motorista'}
+                                onChange={e => this.onChangeAddTravelInfo('driver_id', e) }
                                 >
-                                    {drivers}
+                               
+                                {this.state.driversData.map(driver => 
+                                <Option key={driver.id} >
+                                
+                                
+                                {driver.name} 
+                                 | CPF: {driver.cpf_number}
+
+                                 </Option>
+ 
+                                )}
                                 </Select> 
                                     
                                 )}
@@ -158,8 +191,18 @@ class addTravel extends Component {
  
                                 <Select
                                 placeholder={'Escolha o veículo'}
+                                showSearch
+                                optionFilterProp="children"
+                                onChange={e => this.onChangeAddTravelInfo('vehicle_id', e) }
                                 >
-                                    {vehicles}
+                                {this.state.vehiclesData.map(vehicle => 
+                                <Option key={vehicle.id}>
+                                {vehicle.brand} 
+                                 | Modelo: {vehicle.model} 
+                                 | Placa: {vehicle.plate}
+                                
+                                </Option>
+                                )}
                                     
                                 </Select> 
                                     
@@ -168,6 +211,7 @@ class addTravel extends Component {
                             </FormItem>
                             </Col>
                             </Row>
+                            
                         </Form>
 
                     </Box>
@@ -187,9 +231,19 @@ class addTravel extends Component {
                                 })(
  
                                 <Select
+                                showSearch
+                                optionFilterProp="children"
                                 placeholder={'Escolha a carreta'}
+                                mode='multiple'
+                                onChange={e => this.onChangeAddTravelInfo('carts_id', e) }
                                 >
-                                    {carts}
+                                  {this.state.cartsData.map(cart => 
+                                  <Option key={cart.id}>
+                                  {cart.brand} 
+                                  | Modelo: {cart.model} 
+                                  | Placa: {cart.plate}
+                                  
+                                  </Option>)}
                                     
                                 </Select> 
                                     
@@ -218,18 +272,106 @@ class addTravel extends Component {
  
                                 <Select
                                 placeholder={'Escolha a rota'}
+                                showSearch
+                                optionFilterProp="children"
+                                onChange={e => this.onChangeAddTravelInfo('itinerary_id', e) }
                                 >
-                                    {itineraries}
+                                {this.state.itinerariesData.map(itinerary => 
+                                    <Option 
+                                    key={itinerary.id}>
+                                    {itinerary.route_name} | 
+                                    {itinerary.initial_point} até {itinerary.end_point}
                                     
+                                    </Option>
+                                      
+                                )}
                                 </Select> 
                                     
                                 )}
-                              
                             </FormItem>
                             </Col>
                             </Row>
                         </Form> 
                     </Box>
+                    <Box>
+                        <h3>Informações complementares</h3>
+                        <Form>
+                        <Row gutter={12}>
+                                <Col sm={24} xs={24} md={7}>
+                                        <FormItem label='Data de partida' hasFeedback>
+                                                {getFieldDecorator('departureDate', {
+                                                     rules: [
+                                                        {
+                                                            required: true,
+                                                            message: 'Campo obrigatório'
+                                                        }
+                                                    ]
+                                                })(
+                                                
+                                                    <Input 
+                                                    type='date'
+                                                    placeholder='Data de partida'
+                                                    name='departureDate'
+                                                    onChange={e => this.onChangeAddTravelInfo('departureDate', e.target.value)}
+                                                />
+                                                    
+                                                )}
+                                        </FormItem>
+                                </Col>
+                                <Col sm={24} xs={24} md={7}>
+                                        <FormItem label='Data de chegada' hasFeedback>
+                                                {getFieldDecorator('arrivalDate', {
+                                                     rules: [
+                                                        {
+                                                            required: true,
+                                                            message: 'Campo obrigatório'
+                                                        }
+                                                    ]
+                                                })(
+                                                
+                                                    <Input 
+                                                    type='date'
+                                                    placeholder='Data de chegada'
+                                                    name='departureDate'
+                                                    onChange={e => this.onChangeAddTravelInfo('arrivalDate', e.target.value)}
+                                                />
+                                                    
+                                                )}
+                                        </FormItem>
+                                </Col>
+                       
+                            <Col sm={24} xs={24} md={7}>
+                            <FormItem label='Status' hasFeedback>
+                                {getFieldDecorator('status', {
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: 'Campo obrigatório'
+                                        }
+                                    ]
+                                })(
+ 
+                                <Select
+                                placeholder={'Status'}
+                                showSearch
+                                optionFilterProp="children"
+                                onChange={e => this.onChangeAddTravelInfo('status', e) }
+                                >
+                                <Option value='in_progress'>Em andamento </Option>
+                                <Option value='finished'>Concluída </Option>
+                                <Option value='canceled'>Cancelada </Option>
+                                </Select> 
+                                    
+                                )}
+                            </FormItem>
+                            </Col>
+                            </Row>
+                        </Form>  
+                    </Box>
+                    
+                    <Button key='submit' type='primary' loading={this.state.confirmLoading} 
+                        onClick={this.addTravel}> Cadastrar 
+                 </Button>
             </Box>
             </LayoutWrapper>
         )
